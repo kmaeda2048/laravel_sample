e6b57d1d021164ee7ec36d927c52ce7fdd4d7871
First-name Family-name <username@example.com>
2020-06-28 17:31:59 +0900

    TaskFactory作成

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Task;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+$factory->define(Task::class, function (Faker $faker) {
+    return [
+        'title' => Str::random(5),
+        'body' => Str::random(10),
+        // 'title' => $faker->title,
+        // 'body' => $faker->paragraph,
+        // 'user_id' => factory(App\User::class),
+    ];
+});

ce148281cc544b7893d7f078a73f2967b50ddc31
First-name Family-name <username@example.com>
2020-06-30 00:20:37 +0900

    TaskFactoryのコメント整理

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Str;
 
 $factory->define(Task::class, function (Faker $faker) {
     return [
-        'user_id' => mt_rand(1, 50),
+        'user_id' => mt_rand(1, 50), // テスト用のユーザーはとりあえず50人つくるという前提
         'title' => Str::random(20),
         'body' => Str::random(80),
+        // 'user_id' => factory(App\User::class), // Userも同時につくる
         // 'title' => $faker->title,
         // 'body' => $faker->paragraph,
-        // 'user_id' => factory(App\User::class),
     ];
 });
